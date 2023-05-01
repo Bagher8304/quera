e909808dca3da445350e9dd4e2ed8fab9ab5ba6f
Mohammad Bagher <mb.zarif8304@gmail.com>
2023-05-01 14:45:17 +0330

    first suc3

--- src/test5.java
+++ src/test5.java
@@ -1,13 +1,8 @@
 import java.util.ArrayList;
 
 public class test5 {
-    String name;
-    int salory;
-    static ArrayList<test5> listofperson= new ArrayList<>();
-    {
-        System.out.println("h");
-        int x=2;
-        //first real commit
+    public static void main(String[] args) {
+        System.out.println();
     }
 
 
