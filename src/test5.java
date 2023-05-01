ecb2b99067642dc1ae5dff2c398ebe6f959a91d7
Mohammad Bagher <mb.zarif8304@gmail.com>
2023-05-01 16:21:27 +0330

    firat commit

--- src/test5.java
+++ src/test5.java
@@ -1,9 +1,15 @@
-import java.util.ArrayList;
-
 public class test5 {
     public static void main(String[] args) {
         System.out.println();
         System.out.println("kiram dahant");
+        avv();
+        System.out.println("why not? ");
+        System.out.println("i am strong");
+
+
+    }
+    public static void avv(){
+
     }
 
 
